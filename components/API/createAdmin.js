159e2104aab49334dea6b21f3a7f0e587eedc19e
0xChicharito <133844082+0xChicharito@users.noreply.github.com>
2024-08-11 13:33:49 +0700

    createAdmin.js

---
+++ components/API/createAdmin.js
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Only POST requests allowed' });
+  }
+
+  try {
+    const admin = await prisma.user.create({
+      data: {
+        email: 'admin@gmail.com', // Thay đổi với email của bạn
+        password: 'admin', // Hash mật khẩu của bạn trước khi lưu
+        role: 'ADMIN', // Gán vai trò ADMIN
+      },
+    });
+    res.status(200).json({ message: 'Admin created', admin });
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating admin', error });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
